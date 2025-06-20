9271c59a8cb7e1a21561d640ce70ce2f3f68fc45
Suraj Gupta <surajgupta7070031833@gmail.com>
2025-06-20 18:34:47 +0530

    Refactor route imports and declarations for better organization

--- 05_Backend_Mega_Project/src/app.js
+++ 05_Backend_Mega_Project/src/app.js
@@ -3,6 +3,15 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import { ApiError } from './utils/ApiError.js';
 
+// Routes import
+import userRouter from './routes/user.routes.js';
+import videoRouter from './routes/video.routes.js';
+import tweetRouter from './routes/tweet.routes.js';
+import commentRouter from './routes/comment.routes.js';
+import likeRouter from './routes/like.routes.js';
+import playlistRouter from './routes/playlist.routes.js';
+import subscriptionRouter from './routes/subscription.routes.js';
+
 const app = express();
 
 app.use(cors({
@@ -15,11 +24,14 @@ app.use(express.urlencoded({extended: true, limit: '16kb'}));
 app.use(express.static('public'));
 app.use(cookieParser());
 
-// Routes import
-import userRouter from './routes/user.routes.js';
-
 // Routes Declaration
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/videos', videoRouter);
+app.use('/api/v1/tweets', tweetRouter);
+app.use('/api/v1/comments', commentRouter);
+app.use('/api/v1/likes', likeRouter);
+app.use('/api/v1/playlists', playlistRouter);
+app.use('/api/v1/subscriptions', subscriptionRouter);
 
 // http:localhost:8080/api/v1/users/register
 
